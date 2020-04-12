95d9f4a3ee6be26da088df7ff5703b95a7f25b14
yaneg-ru <yaneg.ru@gmail.com>
2020-04-12 12:44:01 +0500

    Customize User Authentication URL

--- src/main/java/ru/yaneg/graduation_of_topjava_springboot/security/WebSecurity.java
+++ src/main/java/ru/yaneg/graduation_of_topjava_springboot/security/WebSecurity.java
@@ -23,11 +23,17 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll()
-                .anyRequest().authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
+                .anyRequest().authenticated().and().addFilter(getAuthenticationFilter());
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    protected AuthenticationFilter getAuthenticationFilter() throws Exception {
+        final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+        filter.setFilterProcessesUrl("/users/login");
+        return filter;
     }
 }
