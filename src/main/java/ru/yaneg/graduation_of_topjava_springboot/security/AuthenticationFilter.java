b0086a2996095c82d90255f9580b8d3b15f61275
yaneg-ru <yaneg.ru@gmail.com>
2020-04-12 11:06:16 +0500

    Implementing Authentication Filter

---
+++ src/main/java/ru/yaneg/graduation_of_topjava_springboot/security/AuthenticationFilter.java
@@ -0,0 +1,74 @@
+package ru.yaneg.graduation_of_topjava_springboot.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.yaneg.graduation_of_topjava_springboot.ui.model.request.UserLoginRequest;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    // private String contentType;
+
+    public AuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req,
+                                                HttpServletResponse res) throws AuthenticationException {
+        try {
+
+            //contentType = req.getHeader("Accept");
+
+            UserLoginRequest creds = new ObjectMapper()
+                    .readValue(req.getInputStream(), UserLoginRequest.class);
+
+            return authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            creds.getEmail(),
+                            creds.getPassword(),
+                            new ArrayList<>())
+            );
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication auth)  {
+
+        String userName = ((User) auth.getPrincipal()).getUsername();
+
+        String token = Jwts.builder()
+                .setSubject(userName)
+                .setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET)
+                .compact();
+        //UserService userService = (UserService)SpringApplicationContext.getBean("userServiceImpl");
+        //UserDto userDto = userService.getUser(userName);
+
+        res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+        //res.addHeader("UserID", userDto.getUserId());
+
+    }
+
+}
